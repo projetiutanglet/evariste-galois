ffeaab3c125d349f3c4f34091e3a4b28a7d14cd3
Loic Barbe <loic.barbe@iutbayonne.univ-pau.fr>
2018-03-01 10:34:03 +0000

    sauvegarde

---
+++ wp-content/themes/amora-child/page-classe.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package amora
+ */
+
+get_header(); ?>
+
+	<?php
+		if(!is_user_logged_in() && $_SERVER['REQUEST_URI'] == "/le-coin-des-parents/") {
+		
+			$adresse = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]/connexion/";
+			wp_redirect( $adresse, 302 );
+		}?>
+	<div id="primary-mono" class="content-area <?php do_action('amora_primary-width') ?> page">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'modules/content/content', 'page' ); ?>
+
+				<?php
+                if( is_front_page() && !get_theme_mod('amora_disable_comments')) :
+
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+            endif;
+				?>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+	
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
