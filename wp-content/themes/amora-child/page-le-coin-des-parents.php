379d6911ddf27b46e0cdbba0e6c351cfdc826b09
Loic Barbe <loic.barbe@iutbayonne.univ-pau.fr>
2018-03-01 17:01:51 +0000

    pages privée + actualités neutre

--- wp-content/themes/amora-child/page-le-coin-des-parents.php
+++ wp-content/themes/amora-child/page-le-coin-des-parents.php
@@ -11,26 +11,24 @@
  */
 
 get_header(); ?>
-<div id="primary-mono" class="content-area <?php do_action('amora_primary-width') ?> page">
-		<main id="main" class="site-main" role="main">
 
+	<?php
+		if(!is_user_logged_in() && $_SERVER['REQUEST_URI'] == "/le-coin-des-parents/") {
+		
+			$adresse = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]/connexion/";
+			wp_redirect( $adresse, 302 );
+		}?>
+	<div id="primary-mono" class="content-area <?php do_action('amora_primary-width') ?> page">
+		<main id="main" class="site-main" role="main">
+			
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'modules/content/content', 'page' ); ?>
-
-				<?php
-                if( is_front_page() && !get_theme_mod('amora_disable_comments')) :
-
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-            endif;
-				?>
 
 			<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+	
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- wp-content/themes/amora-child/page-orange.php
+++ wp-content/themes/amora-child/page-orange.php
@@ -20,21 +20,13 @@ get_header(); ?>
 		}?>
 	<div id="primary-mono" class="content-area <?php do_action('amora_primary-width') ?> page">
 		<main id="main" class="site-main" role="main">
-
+			
+			
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'modules/content/content', 'page' ); ?>
 
-				<?php
-                if( is_front_page() && !get_theme_mod('amora_disable_comments')) :
-
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-            endif;
-				?>
-
+			
 			<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->
